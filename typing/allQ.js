91e45fa79d62461740474ded742d83c1836c300c
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-08-13 23:42:33 +0900

    Add files via upload

---
+++ typing/allQ.js
@@ -0,0 +1,20 @@
+let allQ = [
+{
+	roma: 'nattounebanebanattoupenn',
+	kana: 'なっとうねばねばなっとうぺん',
+	main: '納豆ネバネバ納豆ペン',
+	fin: '',
+},
+{
+	roma: 'penpainappuruappurupenn',
+	kana: 'ぺんぱいなっぷるあっぷるぺん',
+	main: 'ペンパイナップルアップルペン',
+	fin: '',
+},
+{
+	roma: 'senkyuuhyakukyuuzyuuhatinenn',
+	kana: 'せんきゅうひゃくきゅうじゅうはちねん',
+	main: '1998年',
+	fin: '',
+},
+]
---
+++ typing/main.js
@@ -0,0 +1,97 @@
+const output = document.getElementById('output'),
+	romaD = document.getElementById('roma'),
+	kanaD = document.getElementById('kana'),
+	mainD = document.getElementById('main')
+
+let chars = '',
+	qn = 0,
+	nowQ = allQ[qn],
+	nokori = nowQ.kana.replace(new RegExp(`^${nowQ.fin}`),'').split(''),
+	sTime = new Date,
+	total = { time:0, type:0, miss:0 }
+
+const nokoriR = () => {
+	nokori = nowQ.kana.replace(new RegExp(`^${nowQ.fin}`), '').split('')
+}
+
+const type = e => {
+	if(allQ.length<=qn) return false//print('あ"？')
+	// 大文字小文字変換入れないとね
+	const key = e.key
+	chars += key
+	const r2hR = r2h(chars)
+	output.innerHTML = `${nowQ.fin + r2hR}`
+	
+	const kanas = r2hR.replace(/[a-z]/g,'').split('')
+	// const kanas = r2hR.match(/[---]/g)
+	if(kanas[0]){
+		for(let i1 = 0, i2 = 0; i1 < kanas.length; i1++){
+			if(kanas[i1] == nokori[i2]){
+				nowQ.fin += kanas[i1];
+				i2++
+			}
+		}
+		nokoriR()
+		//output.innerHTML = ``;
+		chars = ''
+	}
+	// print(nokori)
+	if(!nokori[0]){
+		qn++
+		total.time += new Date - sTime
+		alert(
+`${qn}問目終了　ーNEXTー＞　${qn+1}問目
+成績：
+> \t---
+> ミス\t---
+> 時間\t${((new Date)-sTime)/1000}秒`)
+		setDisplay()
+		sTime = new Date
+	}
+	return false;
+}
+
+const setDisplay = () => {
+	nowQ = allQ[qn]
+	if(!nowQ) return finish()
+	nokoriR()
+	const position = $(`#Q${qn}`).offset().top - 100;
+    $('body').animate({scrollTop:position}, 500, "swing");
+}
+
+const start = () => {
+	alert('スタートします')
+	
+	document.addEventListener('keypress', type)
+	// setDisplay()
+	for (let i = 0; i < allQ.length; i++) {
+		document.querySelector('main').innerHTML += `
+		<div id="Q${i}" class="Q">
+			<p class="roma">${allQ[i].roma}</p>
+			<p class="kana">${allQ[i].kana}</p>
+			<p class="main">${allQ[i].main}</p>
+		</div>`
+	}
+	sTime = new Date
+	const position = $(`#Q${qn}`).offset().top - 100;
+    $('body').animate({scrollTop:position}, 500, "swing");
+}
+
+const finish = () => {
+	document.removeEventListener('keypress', type);
+	alert(
+`全問終了！
+合計成績：
+> タイプ\t---
+> ミス\t---
+> 時間\t${total.time/1000}秒
+> スコア\t---`)
+}
+
+
+/*
+document.addEventListener('keyup', e => {
+	document.getElementById('output').innerHTML = '';
+	return false;
+});
+*/
