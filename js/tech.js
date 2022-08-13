5dfc32efa86684dc67d70d6ae827b861602b7b0c
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-08-14 02:26:57 +0900

    Update tech.js

--- js/tech.js
+++ js/tech.js
@@ -1,21 +1,31 @@
+let conf = {}
 const tech = {
-    // 設定
-    set: {
-        // ヘッダーテキスト
-        headerText: (text) => {
-            const a = $('#headerText')[0];
-            if (text != undefined) a.innerHTML = text
-            return a.innerHTML
-        },
-        // アプリケーションの名前
-        appName: () => {
+    // ヘッダーテキスト
+    headerText: (text) => {
+        const a = $('#headerText')[0];
+        if (text != undefined) conf.headerText = text , a.innerHTML = text
+        return a.innerHTML
+    },
+    // アプリケーションの名前
+    appName: (text) => {
+        if (text != undefined) conf.appName = text
+        return conf.appName
+    },
+    // タイトル
+    title: (text) => {
+        if (text != undefined) conf.title = text , document.title = `${text} - ${tech.appName()} | 技術科部`
+        return conf.title
+    },
+    // 作者
+    author: (text) => {
+        if (text != undefined) conf.author = text
+        return conf.author
+    },
+    // GoogleAuth
+    GoogleAuth: {
+        login: (path) => {
             
         },
-        // タイトル
-        title: (text) => {
-            text
-        },
-        author: (name) => {
-        },
+        account: () => {},
     },
 }
