4f2f3c856fdbfad3b61c85b42de3dfb0fb982357
Topi <84213227+apple-pi-yhhhh@users.noreply.github.com>
2022-08-28 00:05:29 +0900

    Create add.php

---
+++ lob/add.php
@@ -0,0 +1,10 @@
+<?php
+$host = 'sv1.php.xdomain.ne.jp';
+$username = 'topi_lob';
+$passwd = 'topi2020';
+$dbname = 'topi_lob';
+$port = '';
+$socket = '';
+mysqli_connect($host,$username,$passwd,$dbname/*,$port,$socket*/);
+
+?>
